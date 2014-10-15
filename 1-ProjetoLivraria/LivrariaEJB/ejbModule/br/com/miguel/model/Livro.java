48d58bbddfa2f476b6bb012c2acf6b543ad549dd
Miguel Suez Vve Penteado <miguel7penteado@gmail.com>
2014-10-15 17:06:02 -0300

    Ajustes

--- 1-ProjetoLivraria/LivrariaEJB/ejbModule/br/com/miguel/model/Livro.java
+++ 1-ProjetoLivraria/LivrariaEJB/ejbModule/br/com/miguel/model/Livro.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.sql.Array;
 
-
 /**
  * The persistent class for the livros database table.
  * 
  */
 @Entity
-@Table(name="livros")
-@NamedQuery(name="Livro.findAll", query="SELECT l FROM Livro l")
-public class Livro implements Serializable {
+@Table(name = "livros")
+@NamedQuery(name = "Livro.localizaTodosLivros", query = "SELECT l FROM Livro l")
+public class Livro implements Serializable
+{
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private Array descricao;
@@ -22,72 +22,80 @@ public class Livro implements Serializable {
 	private Array preco;
 	private Array titulo;
 
-	public Livro() {
+	public Livro()
+	{
 	}
 
-
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	public Integer getId() {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Integer getId()
+	{
 		return this.id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Integer id)
+	{
 		this.id = id;
 	}
 
-
-	public Array getDescricao() {
+	public Array getDescricao()
+	{
 		return this.descricao;
 	}
 
-	public void setDescricao(Array descricao) {
+	public void setDescricao(Array descricao)
+	{
 		this.descricao = descricao;
 	}
 
-
-	public Boolean getIlustrado() {
+	public Boolean getIlustrado()
+	{
 		return this.ilustrado;
 	}
 
-	public void setIlustrado(Boolean ilustrado) {
+	public void setIlustrado(Boolean ilustrado)
+	{
 		this.ilustrado = ilustrado;
 	}
 
-
-	public Array getIsbn() {
+	public Array getIsbn()
+	{
 		return this.isbn;
 	}
 
-	public void setIsbn(Array isbn) {
+	public void setIsbn(Array isbn)
+	{
 		this.isbn = isbn;
 	}
 
-
-	@Column(name="numero_paginas")
-	public Integer getNumeroPaginas() {
+	@Column(name = "numero_paginas")
+	public Integer getNumeroPaginas()
+	{
 		return this.numeroPaginas;
 	}
 
-	public void setNumeroPaginas(Integer numeroPaginas) {
+	public void setNumeroPaginas(Integer numeroPaginas)
+	{
 		this.numeroPaginas = numeroPaginas;
 	}
 
-
-	public Array getPreco() {
+	public Array getPreco()
+	{
 		return this.preco;
 	}
 
-	public void setPreco(Array preco) {
+	public void setPreco(Array preco)
+	{
 		this.preco = preco;
 	}
 
-
-	public Array getTitulo() {
+	public Array getTitulo()
+	{
 		return this.titulo;
 	}
 
-	public void setTitulo(Array titulo) {
+	public void setTitulo(Array titulo)
+	{
 		this.titulo = titulo;
 	}
 
